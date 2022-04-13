89639d555affd929a3357e4b82c8be29c1338dd2
Mark Hamstra <hello@markhamstra.com>
2022-04-14 00:56:32 +0200

    Update paragonie/random_compat to 2.0.21

--- core/components/csrfhelper/vendor/composer/autoload_static.php
+++ core/components/csrfhelper/vendor/composer/autoload_static.php
@@ -24,11 +24,16 @@ class ComposerStaticInit8e4aeba44501ab5543d62ecdebbec7b7
         ),
     );
 
+    public static $classMap = array (
+        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit8e4aeba44501ab5543d62ecdebbec7b7::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit8e4aeba44501ab5543d62ecdebbec7b7::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit8e4aeba44501ab5543d62ecdebbec7b7::$classMap;
 
         }, null, ClassLoader::class);
     }
---
+++ core/components/csrfhelper/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '76e17b31e765fde03f3c8a0f1d78edae1b69d0e2',
+        'name' => 'modmore/csrf-helper',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'modmore/csrf-helper' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '76e17b31e765fde03f3c8a0f1d78edae1b69d0e2',
+            'dev_requirement' => false,
+        ),
+        'paragonie/random_compat' => array(
+            'pretty_version' => 'v2.0.21',
+            'version' => '2.0.21.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../paragonie/random_compat',
+            'aliases' => array(),
+            'reference' => '96c132c7f2f7bc3230723b66e89f8f150b29d5ae',
+            'dev_requirement' => false,
+        ),
+    ),
+);
